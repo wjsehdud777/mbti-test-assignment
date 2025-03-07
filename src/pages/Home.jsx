05ee00bd16224c93bfa94b1c68cb0e77d14b96ad
JDY <wjsehdud@nate.com>
2025-03-07 21:51:45 +0900

    Project: 스타일 개선: 홈 페이지 제목에 글꼴 및 크기 추가, CSS에 루트 스타일 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>MBTI 테스트에 오신 것을 환영합니다!</h1>
+      <h1 className="font-bold text-2xl">MBTI 테스트에 오신 것을 환영합니다!</h1>
       <p>자신의 성격 유형을 확인해보세요.</p>
     </div>
   );
